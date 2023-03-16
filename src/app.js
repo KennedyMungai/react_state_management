555c3228e58805991d9da448c4ef2d3e6f177ff6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-16 14:15:05 +0300

    Rendered out the AddPostForm component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import AddPostForm from './features/posts/AddPostForm'
 import PostsList from './features/posts/postsList'
 
 const App = () =>
 {
     return (
         <div>
+            <AddPostForm />
             <PostsList />
         </div>
     )
