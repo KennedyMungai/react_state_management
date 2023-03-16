7944e55fc511f1d10f8f69db90b8105603a10c3a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-16 15:35:35 +0300

    Added some code and logic to the TimeAgo arrow function

--- src/features/posts/TimeAgo.js
+++ src/features/posts/TimeAgo.js
@@ -4,8 +4,21 @@ import React from 'react'
 
 const TimeAgo = ({ timestamp }) =>
 {
+    let timeAgo = ''
+
+    if (timestamp)
+    {
+        const date = parseISO(timestamp)
+        const timePeriod = formatDistanceToNow(date)
+        timeAgo = `${timePeriod} ago`
+    }
+
     return (
-        <div>TimeAgo</div>
+        <span
+            title={timestamp}
+        >
+            &nbsp; <i>{timeAgo}</i>
+        </span>
     )
 }
 
