d2a18777edb02eaa9a1e04b308e9f3ee1feea71d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-16 12:44:54 +0300

    Created the initialState array

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,3 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
+const initialState = [
+    {
+        id: '1',
+        title: 'Microsoft',
+        content: 'Bill Gates is a bitch nigga'
+    },
+    {
+        id: '2',
+        title: 'Meta',
+        content: 'Mark Zuckerburg is scary'
+    }
+]
