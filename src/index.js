8e2c89c80d3c462084b780bef67b2d197824019f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-16 11:21:21 +0300

    Set up the provider and the store

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import { store } from './app/store'
 import './index.css'
 
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById("root"))
