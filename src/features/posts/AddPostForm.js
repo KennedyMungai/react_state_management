ef0f1e36413cb513ac318956129695ecf3c2e037
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-16 14:00:14 +0300

    Created a label and an accompanying input tag

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -5,12 +5,21 @@ const AddPostForm = () =>
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
 
-    const onTitleChanges = e => setTitle(e.target.value)
+    const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value)
 
     return (
         <section>
             <h2>Add a New Post</h2>
+            <form >
+                <label htmlFor="postTitle">Post Title: </label>
+                <input
+                    type="text"
+                    name="postTitle"
+                    id="postTitle"
+                    onChange={onTitleChanged}
+                />
+            </form>
         </section>
     )
 }
