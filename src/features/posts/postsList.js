7024a8004af6c0396c6342b876784a65757bf00b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-16 12:53:36 +0300

    Created the rendered posts const which contains the markup for all the rendered out posts

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -6,6 +6,15 @@ const PostsList = () =>
 {
     const posts = useSelector(state => state.posts)
 
+    const renderedPosts = posts.map((post) => (
+        <article
+            key={post.id}
+        >
+            <h3>{post.title}</h3>
+            <p>{post.content.substring(0, 100)}</p>
+        </article>
+    ))
+
     return (
         <div>postsList</div>
     )
