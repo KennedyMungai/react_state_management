aadce770191dbfe0f2ebb1c6b8799d2357edaa95
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-16 14:46:14 +0300

    Added userId to the prepare function

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -24,7 +24,7 @@ const postSlice = createSlice({
             {
                 state.push(action.payload)
             },
-            prepare(title, content)
+            prepare(title, content, userId)
             {
                 return {
                     payload: {
