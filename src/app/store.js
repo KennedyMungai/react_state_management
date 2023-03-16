1dba07dbbf1dfc791a701feb90c1ad59aa9ac9ec
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-16 11:37:52 +0300

    Added the comma for reaons

--- src/app/store.js
+++ src/app/store.js
@@ -5,6 +5,6 @@ import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
     }
 })
