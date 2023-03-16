c941a16720e1523c846d63dcd49d959f6dfb8f56
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-16 14:41:28 +0300

    Created the skeleton for the userSlice reducer

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,3 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = []
+
+
+const userSlice = createSlice(
+    {
+        name: "users",
+        initialState,
+        reducers: {
+
+        }
+    }
+)
+
+const userReducer = userSlice.reducer
+
+export default userReducer
