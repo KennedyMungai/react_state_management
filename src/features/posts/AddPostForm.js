8c802b7a95c6c0bca33a8947aaf59a86318eb690
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-16 14:49:10 +0300

    Created the userId state

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -10,6 +10,7 @@ const AddPostForm = () =>
 
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
+    const [userId, setUserId] = useState('')
 
     const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value)
